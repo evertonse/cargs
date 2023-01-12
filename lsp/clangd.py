678be4842a435883500886c250bbcca3b7998b8a
Everton Jr <evertonse.junior@gmail.com>
2023-01-12 00:27:45 -0300

    @added support for clangd defines

--- lsp/clangd.py
+++ lsp/clangd.py
@@ -3,12 +3,13 @@ from project import Project
 
 def create_config_file(project : Project) -> str:
     relative_includes = ['-I'+ str(Path(include)).replace('\\','/') for include in project.includedirs]
+    defines   = ['-D' + str(d) for d in project.defines]
     directory = str(project.workingdir.resolve()).replace('\\','/')
     config_str = f"""
 [
     {{
         "directory":"{directory}"
-        "command" : "g++ {' '.join(relative_includes)}"
+        "command" : "g++  {' '.join(defines)} {' '.join(relative_includes)}"
         "file": ""
     }}
 ]
